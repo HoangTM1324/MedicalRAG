08b15573467111c4534a6a8dbe2de8eec8a5254c
Thach Minh Hoang <thachminhhoang132004@gmail.com>
2026-01-14 18:02:29 +0700

    Add configuration for model and dataset

---
+++ src/config.py
@@ -0,0 +1,10 @@
+import os
+
+# Model & Data Config
+MODEL_NAME = "Qwen/Qwen2.5-0.5B"
+EMBEDDING_MODEL_NAME = "pritamdeka/S-PubMedBert-MS-MARCO"
+DATASET_NAME = "qiaojin/PubMedQA"
+
+# Paths
+CHROMA_PATH = "./chroma_db_storage"
+OUTPUT_DIR = "./qwen-finetuned-output"
